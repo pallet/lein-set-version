de91d005d72536bb8b7a5b2783f3002a2d60e5b8
Hugo Duncan <hugo@hugoduncan.org>
2012-09-04 22:18:52 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-set-version "0.2.1"
+(defproject lein-set-version "0.2.2-SNAPSHOT"
   :description "A Leiningen plugin to update the project version."
   :url "https://github.com/pallet/lein-set-version"
   :scm {:url "git@github.com:pallet/lein-set-version.git"}
